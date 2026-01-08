30054cd31e852ed586cee90d04cd2749fd102a17
Michael Oberman <obermanmichael6@gmail.com>
2026-01-08 10:12:39 +0200

    feat: add audit auth incident monitoring controller

---
+++ backend/gateway/src/controllers/audit.controller.ts
@@ -0,0 +1,19 @@
+//TODO check protocol
+
+import { Request, Response, NextFunction } from 'express';
+import { auditServiceAWSLambda } from '../services/impl/AuditServiceImplAWSLambda.js';
+
+class AuditController {
+
+    async sendAuditEvent(req: Request, res: Response, next: NextFunction) {
+        try {
+            const result = await auditServiceAWSLambda.sendAuditEvent(req.body);
+            res.status(201).send(result);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+export const auditController = new AuditController();
+
