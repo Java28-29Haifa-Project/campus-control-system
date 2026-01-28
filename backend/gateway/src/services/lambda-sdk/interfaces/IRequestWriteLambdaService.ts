567348a65b0e0e75c219304dea0d587e99dda13f
Michael Oberman <obermanmichael6@gmail.com>
2026-01-28 21:08:40 +0200

    feat: add updateRequestStatus to IRequestWriteLambdaService interface

--- backend/gateway/src/services/lambda-sdk/interfaces/IRequestWriteLambdaService.ts
+++ backend/gateway/src/services/lambda-sdk/interfaces/IRequestWriteLambdaService.ts
@@ -3,9 +3,13 @@ import {
     UpdateRequestInputLambda,
     RequestLambdaResponse
 } from './IRequestLambdaService.js';
+import {UpdateRequestStatusInputLambda} from "../../../types/ticketRequest.js";
 
 export interface IRequestWriteLambdaService {
     createRequest(input: CreateRequestInputLambda): Promise<RequestLambdaResponse>;
     updateRequest(input: UpdateRequestInputLambda): Promise<RequestLambdaResponse>;
+    updateRequestStatus(
+        input: UpdateRequestStatusInputLambda
+    ): Promise<RequestLambdaResponse>;
     healthCheck(): Promise<{ service: string; status: string; timestamp: string }>;
 }
