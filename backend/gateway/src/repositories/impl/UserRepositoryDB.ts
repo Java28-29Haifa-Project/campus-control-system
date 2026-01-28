88e40e7ecc102294df42c7d6cd772299987eacbb
Michael Oberman <obermanmichael6@gmail.com>
2026-01-28 19:41:10 +0200

    fix: add returning type for create user repo method, add crypto rendom UUID as user id, map username field for db

--- backend/gateway/src/repositories/impl/UserRepositoryDB.ts
+++ backend/gateway/src/repositories/impl/UserRepositoryDB.ts
@@ -1,7 +1,8 @@
-import { Pool } from 'pg';
-import { db } from '../../utils/db.client.js';
+import {Pool} from 'pg';
+import {db} from '../../utils/db.client.js';
 import bcrypt from 'bcrypt';
 import {IUserRepository} from "../IUserRepository.js";
+import {User} from "../../types/auth.js";
 
 class UserRepositoryDB implements IUserRepository {
     private pool: Pool;
@@ -10,20 +11,36 @@ class UserRepositoryDB implements IUserRepository {
         this.pool = pool;
     }
 
-    async createUser(name: string, email: string, password: string) {
+    async createUser(
+        name: string,
+        email: string,
+        password: string
+    ): Promise<User> {
         const passwordHash = await bcrypt.hash(password, 10);
 
         const query = `
-            INSERT INTO users (name, email, password_hash)
-            VALUES ($1, $2, $3)
-            RETURNING user_id AS "userId", name, email, role, created_at AS "createdAt";
+            INSERT INTO users (user_id, username, email, password_hash)
+            VALUES ($1, $2, $3, $4) RETURNING 
+            user_id AS "userId",
+            username,
+            email,
+            role;
         `;
-        const result = await this.pool.query(query, [name, email, passwordHash]);
+
+        const result = await this.pool.query(query, [
+            crypto.randomUUID(),
+            name,
+            email,
+            passwordHash
+        ]);
+
         return result.rows[0];
     }
 
     async findByEmail(email: string) {
-        const query = `SELECT * FROM users WHERE email = $1`;
+        const query = `SELECT *
+                       FROM users
+                       WHERE email = $1`;
         const result = await this.pool.query(query, [email]);
         return result.rows[0] || null;
     }
